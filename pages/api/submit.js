db781dbb624efc6ea8ba6b9e10dbe5ca2101954f
gauthier-th <lord.morgoth@yahoo.com>
2021-10-03 12:23:43 +0200

    Various fixes

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,4 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
 export default function handler(req, res) {
   console.log("new response!", req.body)
-  res.status(200).json({ ok: true })
+  try {
+    const body = JSON.parse(req.body)
+    fs.writeFileSync(
+      path.join(process.env.FORM_SAVES_PATH, `${new Date().getFullYear().toString().substr(-2)}BE${body.numIntervention}-${Date.now()}.json`),
+      req.body
+    )
+    res.status(200).json({ ok: true })
+  }
+  catch {
+    res.status(400).json({ ok: false })
+  }
 }
